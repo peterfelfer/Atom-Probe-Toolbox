671a47486c23ff702b89df77f28ca12ca6da5a21
ValentinAPT <62693841+ValentinAPT@users.noreply.github.com>
2020-07-06 13:47:58 +0200

    addition of movieCreateTurntableAnimation

---
+++ movieCreateTurntableAnimation.m
@@ -0,0 +1,44 @@
+function mov = movieCreateTurntableAnimation(deg,frameRate,fileName)
+% movieCreateTurntableAnimation creates a turntable animation of the current 
+% figure and returns movie variable. The movie is saved in an *.avi format.
+%
+% mov = movieCreateTurntableAnimation(deg,fileName)
+%
+% INPUTS
+% deg:       step size in degree in which the animation rotates. determines
+%            the length and speed of the film.
+% 
+% frameRate: frame rate of the movie in frames per second
+% 
+% fileName:  name under which the film is saved.
+%
+%
+%
+% OUTPUT
+%           movie of the turning atom probe tip.
+
+
+if ~exist('fileName','var')
+    [file path] = uiputfile('*.avi','export movie');
+    fileName = [path file];
+end
+
+set(gcf,'Color','w');
+axis vis3d
+
+nFrames = 360/deg;
+
+for f = 1:nFrames
+    frame = getframe(gcf);
+    mov(f) = frame;
+    camorbit(deg,0);
+end
+
+myVideo = VideoWriter(fileName);
+
+myVideo.FrameRate = frameRate;  
+myVideo.Quality = 100;          % Default 100
+
+open(myVideo);
+writeVideo(myVideo, mov);
+close(myVideo);
