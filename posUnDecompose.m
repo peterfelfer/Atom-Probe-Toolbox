6d1e16a3524975809cf7f71eb23c0fc54e0e4605
ValentinAPT <62693841+ValentinAPT@users.noreply.github.com>
2020-07-21 08:44:24 +0200

    finished documentation

--- posUnDecompose.m
+++ posUnDecompose.m
@@ -1,12 +1,16 @@
 function pos = posUnDecompose(pos)
-% takes a decomposed pos table variable and turns it into a state with
-% only ranges allocated
+% posUnDecompose takes a decomposed pos table variable and turns it into a 
+% state with only ranges allocated
 %
-% INPUT: 
+% INPUT 
 % pos: decomposed pos file or not decomposed file with ranges allocated
 %
-% OUTPUT: 
+% OUTPUT 
 % pos: undecomposed file with only the ranges allocated to the ions
+%
+% NOTE: the output is the same as the result of the function
+% posAllocateRange when using the initial pos file (posIn) and the option
+% 'raw' as inputs
 
 [~, uniqueIonIdx] = unique(pos.ionIdx);
 
