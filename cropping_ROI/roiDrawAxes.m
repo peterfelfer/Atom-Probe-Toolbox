fa3dc2badd80bf92a62d3e43427b3b3a5a7dd4fb
ValentinAPT <62693841+ValentinAPT@users.noreply.github.com>
2020-07-22 15:57:49 +0200

    Hoffentlich zerstöre ich benedicts arbeit nicht

---
+++ cropping_ROI/roiDrawAxes.m
@@ -0,0 +1,15 @@
+function lh = roiDrawAxes(roi)
+% roiDrawAxes draws lines denoting the analysis axes for the ROI object red = x, 
+% green = y, blue = z
+%
+% bh = roiDrawAxes(roi)
+%
+% INPUTS
+% roi: name of roi as given before
+%
+% OUTPUTS
+% bh:   handle to the axes of the roi, colors as described before
+%
+lh(1) = line(roi.UserData.ROIxaxis(:,1),roi.UserData.ROIxaxis(:,2),roi.UserData.ROIxaxis(:,3),'LineWidth',4,'Color','r');
+lh(2) = line(roi.UserData.ROIyaxis(:,1),roi.UserData.ROIyaxis(:,2),roi.UserData.ROIyaxis(:,3),'LineWidth',4,'Color','g');
+lh(3) = line(roi.UserData.ROIzaxis(:,1),roi.UserData.ROIzaxis(:,2),roi.UserData.ROIzaxis(:,3),'LineWidth',4,'Color','b');
