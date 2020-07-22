3f433b8fa0e7ae45a7b8caa50adaafa1d6baa43e
peterfelfer <peter.felfer@fau.de>
2020-07-22 14:57:04 +0200

    hdf5 writer update

--- posTableToHDF5.m
+++ posTableToHDF5.m
@@ -17,13 +17,34 @@ fid = H5F.create(fileName);
 
 
 %% creating dataset groups
-
 % extracting groups form metadata list
-groupsTmp = string(meta{:,1}); 
+% group list needs to be created such that a group is created before its
+% subgroup. This demands extracting the individual components of each path
+
+% deleting the individual variable names
+groupsTmp = string(metaData(:,1)); 
 delPos = regexp(groupsTmp,"/");
 delPos = cellfun(@(x) x(end),delPos);
-groups = extractBefore(groupsTmp,delPos+1);
+groupsTmp = extractBefore(groupsTmp,delPos+1);
+groupsTmp = unique(groupsTmp);
 
+% extracting the group hierachy
+delPos = regexp(groupsTmp,"/");
+groups = [];
+for gr = 1:length(groupsTmp)
+    groupDepth = length(delPos{gr});
+    tmp = repmat(groupsTmp(gr),groupDepth,1);
+    tmp = extractBefore(tmp,(delPos{gr})');
+    groups = [groups; tmp];
+end
+
+% format group list
+groups = unique(groups);
+groups = sort(groups);
+groups(groups == "") = [];
+groups = groups + "/";
+
+% cretaing individual groups
 for gr = 1:length(groups)
     groupID(gr) = H5G.create(fid,groups(gr),...
         'H5P_DEFAULT','H5P_DEFAULT','H5P_DEFAULT');
@@ -61,37 +82,85 @@ for m = 1:size(metaData,1)
             data = metaData{m,2};
         end
         
-        h5writeatt(fileName,path,attribute,data);
-    end
-    
-    
+        h5writeatt(fileName,path,attribute,data,'TextEncoding','UTF-8');
+    end      
 end
 
+H5F.close(fid);
 
-%% writing pos data
+
+%% writing pos data ==> APT specific!
+numEntries = height(pos);
+isDecomposed = numEntries > max(pos.ionIdx); % check if pos file is decomposed
+
+if isDecomposed
+    dataPath = '/atomProbeTomography/reconstruction/atom/';
+else
+    dataPath = '/atomProbeTomography/reconstruction/ion/';
+end
+
 posColumnNames = pos.Properties.VariableNames;
-for col = 1:width(pos)
-    dataSetName = [dataPath posColumnNames{col}]; % a dataset in HDF5 is one consistent piece of data. i.e. each column of the pos variable is one 'dataset'
-    
+for col = 1:width(pos)    
     % formatting data for hdf5 wrtie if necessary
-    if ismember(posColumnNames{col},{'x','y','z','mc'}) %float types
+    if ismember(posColumnNames{col},{'x','y','z'}) %float type coords
         data = table2array(pos(:,col));
-        dataType = 'single';
-        dataPath = '/atomProbeTomography/reconstruction/';
-        type_id = H5T.copy('H5T_NATIVE_SINGLE');
+        h5create(fileName,[dataPath posColumnNames{col}],[numEntries 1]);
+        h5write(fileName,[dataPath posColumnNames{col}],data);
+        h5writeatt(fileName,[dataPath posColumnNames{col}],'unit','nm','TextEncoding','UTF-8');
         
-        H5D.write(dset_id,'H5ML_DEFAULT','H5S_ALL','H5S_ALL',plist,data);
-        H5D.close(dset_id);
-        
-    elseif ismember(posColumnNames{col},{'ionIdx','chargeState','isotope','ionComplexity'}) %integer types
+    elseif ismember(posColumnNames{col},{'chargeState','isotope','ionComplexity'}) %float type coords
         data = table2array(pos(:,col));
-        dataType = 'uint32';
+        h5create(fileName,[dataPath posColumnNames{col}],[numEntries 1]);
+        h5write(fileName,[dataPath posColumnNames{col}],data);
+        
+    elseif ismember(posColumnNames{col},{'mc'})
+        data = table2array(pos(:,col));
+        h5create(fileName,[dataPath 'massToChargeState'],[numEntries 1]);
+        h5write(fileName,[dataPath 'massToChargeState'],data);
+        h5writeatt(fileName,[dataPath 'massToChargeState'],'unit','Da','TextEncoding','UTF-8');
+        
+    elseif ismember(posColumnNames{col},{'ionIdx'})
+        data = table2array(pos(:,col));
+        h5create(fileName,[dataPath 'fieldEvaporationSequenceIndex'],[numEntries 1]);
+        h5write(fileName,[dataPath 'fieldEvaporationSequenceIndex'],data);
         
     elseif ismember(posColumnNames{col},{'ion','atom'}) % categorical arrays converted to string arrays
         %data = string(table2array(pos(:,col)));
         %dataType = 'string';
         data = [1];
         dataType = 'single';
+        
+        
+        % epos variables
+    elseif ismember(posColumnNames{col},{'tof'})
+        data = table2array(pos(:,col));
+        h5create(fileName,'/atomProbeTomography/detectorEvent/timeOfFlight/',[numEntries 1]);
+        h5write(fileName,'/atomProbeTomography/detectorEvent/timeOfFlight/',data);
+        h5writeatt(fileName,'/atomProbeTomography/detectorEvent/timeOfFlight/','unit','ns','TextEncoding','UTF-8');
+        
+    elseif ismember(posColumnNames{col},{'VDC'})
+        data = table2array(pos(:,col));
+        h5create(fileName,'/atomProbeTomography/detectorEvent/standingVoltage',[numEntries 1]);
+        h5write(fileName,'/atomProbeTomography/detectorEvent/standingVoltage',data);
+        h5writeatt(fileName,'/atomProbeTomography/detectorEvent/standingVoltage','unit','V','TextEncoding','UTF-8');
+        
+    elseif ismember(posColumnNames{col},{'VP'})
+        data = table2array(pos(:,col));
+        h5create(fileName,'/atomProbeTomography/detectorEvent/voltagePulseAmplitude/',[numEntries 1]);
+        h5write(fileName,'/atomProbeTomography/detectorEvent/voltagePulseAmplitude/',data);
+        h5writeatt(fileName,'/atomProbeTomography/detectorEvent/voltagePulseAmplitude/','unit','V','TextEncoding','UTF-8');
+        
+    elseif ismember(posColumnNames{col},{'detx'})
+        data = table2array(pos(:,col));
+        h5create(fileName,'/atomProbeTomography/detectorEvent/x/',[numEntries 1]);
+        h5write(fileName,'/atomProbeTomography/detectorEvent/x/',data);
+        h5writeatt(fileName,'/atomProbeTomography/detectorEvent/x/','unit','mm','TextEncoding','UTF-8');
+        
+    elseif ismember(posColumnNames{col},{'dety'})
+        data = table2array(pos(:,col));
+        h5create(fileName,'/atomProbeTomography/detectorEvent/y/',[numEntries 1]);
+        h5write(fileName,'/atomProbeTomography/detectorEvent/y/',data);
+        h5writeatt(fileName,'/atomProbeTomography/detectorEvent/y/','unit','mm','TextEncoding','UTF-8');
         
     end
 
@@ -111,4 +180,4 @@ end
 
 
 %% close file
-H5F.close(fid);
+%H5F.close(fid);
