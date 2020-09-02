a7d0b0049846cc1eb9febe0594f79e2209be89a6
Dalbauer <oz88iqyg@fauad.fau.de>
2020-09-02 14:13:35 +0200

    small changes in documentation

--- roiDrawAxes.m
+++ roiDrawAxes.m
@@ -4,11 +4,11 @@ function lh = roiDrawAxes(roi)
 %
 % bh = roiDrawAxes(roi)
 %
-% INPUTS
-% roi: name of roi as given before
+% INPUT
+% roi:          name of roi as given before
 %
-% OUTPUTS
-% bh:   handle to the axes of the roi, colors as described before
+% OUTPUT
+% bh:           handle to the axes of the roi, colors as described before
 %
 lh(1) = line(roi.UserData.ROIxaxis(:,1),roi.UserData.ROIxaxis(:,2),roi.UserData.ROIxaxis(:,3),'LineWidth',4,'Color','r');
 lh(2) = line(roi.UserData.ROIyaxis(:,1),roi.UserData.ROIyaxis(:,2),roi.UserData.ROIyaxis(:,3),'LineWidth',4,'Color','g');
