21d1310bf14ac8cedc68f56b8417feb855fa6179
Dalbauer <oz88iqyg@fauad.fau.de>
2020-09-02 11:34:11 +0200

    correction of typos and others

--- ionsCreateComplexFromList.m
+++ ionsCreateComplexFromList.m
@@ -1,30 +1,30 @@
 function ionList = ionsCreateComplexFromList(ions,chargeStates,isotopeTable)
-% ionsCreateComplexFromList crates a list of all complex ions and their
+% ionsCreateComplexFromList creates a list of all complex ions and their
 % isotopic combinations from a list of known ions (without isotopic
 % information). The information about isotope abundance and existance is
 % provided via the isotopeTable
-% The data for ions and chargeStates can be imported form the curated, 
-% maintained Excel file in the toolbox via the ionsImportFromXLS
+% The data for ions and chargeStates can be imported from the created, 
+% maintained Excel file in the toolbox via the function ionsImportFromXLS
 %
 % ionList = ionsCreateComplexFromList(ion,chargeStates,isotopeTable)
 %
 % INPUT
-% ions:         list of ions as a string vector ot char cell array, e.g. 
+% ions:         list of ions as a string vector or char cell array, e.g. 
 %               ["Ni O"; "Fe2 O3";...] or {'Ni O'; 'Fe2 O3';...}
 %
 % chargeStates: N x M array of charge states the ion can be found in. M
 %               must be the same as the length of the ions list. M is the
-%               number of chargestates. Entries are true or false for the
-%               chargestate
+%               number of charge states. Entries are true or false for the
+%               charge state
 %
 % isotopeTable: isotopeTable used for the analysis
 %
 %
 % OUTPUT
-% ionList:      table that contians the possible individual peaks. 
+% ionList:      table that contains the possible individual peaks. 
 %               Table with fields:
 %               ion, ionIsotopic: Categorical with ion names
-%               mc: mass to chargestate value of the individual isotopic
+%               mc: mass to charge state value of the individual isotopic
 %               combination in amu
 %
 % TODO: permn creates error on large permutations (e.g. Si17)
@@ -39,9 +39,9 @@ wb = waitbar(0,'building isotopic combinations');
 
 for i = 1:size(ions,1)
     ionIsotopicTmp = {};
-    ionChargeStateTmp = find(chargeStates(i,:)); %charge states for the particular ion
+    ionChargeStateTmp = find(chargeStates(i,:)); % charge states for the particular ion
     
-    %create isotope combination list with weights for each elemental permutation
+    % create isotope combination list with weights for each elemental permutation
     [isoCombos, ~, weightTmp] = ionsCreateIsotopeList(ions(i), isotopeTable);
     
     for it = 1:length(isoCombos)
