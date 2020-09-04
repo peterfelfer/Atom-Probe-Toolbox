bc16ae728014e4b32d7a416e63ead306fc25adfb
ValentinAPT <62693841+ValentinAPT@users.noreply.github.com>
2020-09-04 13:22:51 +0200

    changes in documentation

--- posDistanceAlongVector.m
+++ posDistanceAlongVector.m
@@ -5,19 +5,19 @@ function distance = posDistanceAlongVector(pos,origin,direction)
 % distance = posDistanceAlongVector(pos,origin,direction)
 %
 % INPUT
-% pos =         pos file with the important data 
+% pos:           pos file with the important data 
 %
-% origin =      origin of the Vector
+% origin:        origin of the vector
 %
-% direction =   direction of the Vector
+% direction:     direction of the vector
 %
 % OUTPUT
-% distance =    distance between entries in a pos file along a specified
-%               vector
+% distance:      distance between entries in a pos file along a specified
+%                vector
 
 points = [pos.x pos.y pos.z];
 points = points - repmat(origin,[height(pos),1]); % re-center origin
 
-direction = direction/norm(direction); %make axis unit length
+direction = direction/norm(direction); % make axis unit length
 
-distance = sum(points.*repmat(direction,[height(pos),1]),2);% dot product with scale axis for projected length
+distance = sum(points.*repmat(direction,[height(pos),1]),2); % dot product with scale axis for projected length
