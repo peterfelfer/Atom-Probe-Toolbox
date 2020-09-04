68dc3a55b18700bc37fa0946f9119aa538ddec5a
ValentinAPT <62693841+ValentinAPT@users.noreply.github.com>
2020-09-04 14:20:48 +0200

    small changes in documentation

--- posDistanceToLine.m
+++ posDistanceToLine.m
@@ -8,16 +8,14 @@ function distance = posDistanceToLine(pos,line)
 %
 %
 % INPUT
-% pos =         pos file 
+% pos:          pos file 
 %
-% line =        is a line object consisting of
+% line:         is a line object consisting of
 %               line.vertices (Nx3) and line.edges (Mx2)
 %
 % OUTPUT
-% distance =    
-
-
-
+% distance:     distance between entries in a pos file along a specified
+%               line    
 
 %% distances are calculated perpendicular to the line elements.
 [lineVector, ~] = lineVectors(line.vertices,line.edges);
@@ -28,12 +26,12 @@ function distance = posDistanceToLine(pos,line)
 % finding closest point for each atomic position
 closest = dsearchn(line.vertices,delaunayn(line.vertices),[pos.x, pos.y, pos.z]);
 
-%vector from atom to closest vertex
+% vector from atom to closest vertex
 vec = [pos.x, pos.y, pos.z] - line.vertices(closest,1:3); 
 vecLen = sqrt(sum(vec.^2,2));
 
-%distance along line vector
+% distance along line vector
 distLV = dot(vec, lineVector(closest,:), 2);
 
-%distance normal to line vector
+% distance normal to line vector
 distance = sqrt(vecLen.^2 - distLV.^2); 
