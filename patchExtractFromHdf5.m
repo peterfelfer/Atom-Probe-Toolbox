0ca9db0fe0ddd003351b1884efe3e57f8f2dbcaa
ValentinAPT <62693841+ValentinAPT@users.noreply.github.com>
2020-10-05 10:35:14 +0200

    documentation

--- patchExtractFromHdf5.m
+++ patchExtractFromHdf5.m
@@ -3,27 +3,29 @@ function [meshObject, WCS] = patchExtractFromHdf5(fileName,objectName)
 % also the corresponding work coordinate system from an hdf5 file. The mesh
 % object can be added to the file using hdf5AddMeshRoi.
 %
-% USAGE: patchExtractFromHdf5(fileName,objectName)
+% patchExtractFromHdf5(fileName,objectName)
 %
-% INPUTS:
-%           fileName = full file name of existing hdf5 file
+% INPUTS
+% fileName:     full file name of existing hdf5 file
 %
-%           objectName = char array (string) of the object name as saved to
-%           the hdf5 file, e.g. 'ROI box'
+% objectName:   char array (string) of the object name as saved to
+%               the HDF5 file, e.g. 'ROI box'
 %
-%           meshObject = object stored in the hdf5 file. The object
-%           may be an nD mesh (mesh, lines, points) as a patch object
-%           The object may contain the following datasets:
-%           vertices... Ix3 list of coordinates of the mesh vertices
-%           faces... NxM (M = 2..4) list of quadrilaterals, triangles, or
-%           lines
-%           faceColor... 1x3 (single color) or Nx3 (per face color)
-%           faceAlpha... 1x1 (single opacity value) or Nx1 (per face
-%           opacity value)
+% meshObject:   object stored in the hdf5 file. The object
+%               may be an nD mesh (mesh, lines, points) as a patch object
+%               The object may contain the following datasets:
+%               vertices... Ix3 list of coordinates of the mesh vertices
+%               faces... NxM (M = 2..4) list of quadrilaterals, triangles, or
+%               lines
+%               faceColor... 1x3 (single color) or Nx3 (per face color)
+%               faceAlpha... 1x1 (single opacity value) or Nx1 (per face
+%               opacity value)
 %
-%           WCS = struct defining a work coordinate system. Fields are
-%           'origin' (1x3 vector) and 'coordinateSystemVectors' (3x3 list
-%           of 3 basis vectors of coordinate system (exx exy exz; eyx eyy eyz; ezx ezy ezz)
+% OUTPUT
+% meshObject:   struct of mesh object with field of faces and vertices
+% WCS:          struct defining a work coordinate system. Fields are
+%               'origin' (1x3 vector) and 'coordinateSystemVectors' (3x3 list
+%               of 3 basis vectors of coordinate system (exx exy exz; eyx eyy eyz; ezx ezy ezz)
 %
 % (c) by Prof. Peter Felfer Group @FAU Erlangen-Nürnberg
 
