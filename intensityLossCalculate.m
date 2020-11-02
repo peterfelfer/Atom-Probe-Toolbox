ef3359a693d50ee345107f81e9c80dc2f759cabd
ValentinAPT <62693841+ValentinAPT@users.noreply.github.com>
2020-11-02 10:12:32 +0100

    Update intensityLossCalculate.m

--- intensityLossCalculate.m
+++ intensityLossCalculate.m
@@ -1,16 +1,16 @@
 function lossTable = intensityLossCalculate(spec)
 % intensityLossCalculate puts the intensity values together and calculates
 % the intensity loss for each isotope in percentage
-
-
+ 
+ 
 %% reorder of mass spec plot, probably not necessary
 massSpecReorderPlot(spec);
-
+ 
 %% a table of all ion stem plot y values
 stemPlots = findobj('Type','Stem');
 % get the y coordinates
 for i = 1:numel(stemPlots);
-    yCo = stemPlots(i).YData
+    yCo = stemPlots(i).YData;
     yCo = yCo';
     onlyPart = yCo;
     allPartsCombined = [onlyPart; yCo];
@@ -18,15 +18,8 @@ for i = 1:numel(stemPlots);
     yCo = table(yCo);
     yCo.Properties.VariableNames{1} = 'y values of ion stem plots';
 end
-
-
-
-
-
-
-
-
-
+ 
+ 
 %% y value of ion stem plot of the isotope and max y value of corresponding range
 ionStem = findobj('Type','Stem');
 numberOfIsotopes = numel(ionStem.YData);
@@ -46,7 +39,8 @@ for i = 1:numberOfIsotopes;
     lossTable.Properties.VariableNames{4} = 'intensity loss [%]';
     
 end
-
-
+ 
+ 
 end
+
 
