8dd29d25d8fb7639a6e1b9e2e6487b60600a79b9
Martina <37100644+TinaHel@users.noreply.github.com>
2021-11-09 10:31:08 +0100

    New metaData Workflow

--- metaDataReadLEAPAttributes.m
+++ metaDataReadLEAPAttributes.m
@@ -17,14 +17,14 @@ intRow = exList(exList.ExperimentID == exNum, :);
 % sort Variables 
 meta(contains(meta(:,1),'specimen/uniqueIdentifier'),2) = intRow.Specimen;
 meta(contains(meta(:,1),'project/identifier'),2) = cellstr(char(intRow.Project));
-meta(contains(meta(:,1),'experiment/specimenTemperatureActual'),2) = num2cell(intRow.TemperatureK);
+meta(contains(meta(:,1),'experiment/specimenTemperatureActual'),2) = num2cell(intRow.Temperature_K_);
 % start time
 t = datetime(intRow.Date, 'Format', 'yyyy-MM-dd HH:mm:ss.SSS');
 date = datestr(t, 'yyyy-mm-dd');
 time = datestr(t, 'HH:MM:SS.FFF');
 meta(contains(meta(:,1),'experiment/beginDateTime'),2) = cellstr([date ' ' time]);
 % end time
-endDateTime = intRow.Date + minutes(intRow.RunTimeHrs*60);
+endDateTime = intRow.Date + minutes(intRow.RunTime_Hrs_*60);
 endDate = datestr(endDateTime, 'yyyy-mm-dd');
 endTime = datestr(endDateTime, 'HH:MM:SS.FFF');
 meta(contains(meta(:,1),'experiment/endDateTime'),2) = cellstr([endDate ' ' endTime]);
@@ -33,14 +33,14 @@ meta(contains(meta(:,1),'experiment/result'),2) = {intRow.Result};
 meta(contains(meta(:,1),'experiment/status'),2) = {'finished'};
 meta(contains(meta(:,1),'experiment/uniqueIdentifier'),2) = intRow.RunID;
 meta(contains(meta(:,1),'experiment/instrumentOperator'),2) = cellstr(char(intRow.Operator));
-if ~isnan(intRow.PulseFraction)
-    meta(contains(meta(:,1),'experiment/pulseFraction'),2) = num2cell(intRow.PulseFraction);
+if ~isnan(intRow.PulseFraction___)
+    meta(contains(meta(:,1),'experiment/pulseFraction'),2) = num2cell(intRow.PulseFraction___);
     meta(contains(meta(:,1),'experiment/pulseType'),2) = {'voltagePulsed'};
 end
-meta(contains(meta(:,1),'experiment/pulsePeriod'),2) = num2cell((1/intRow.PulseFrequencyHz)*10^9);
-meta(contains(meta(:,1),'instrument/flightPathLength'),2) = num2cell(intRow.FlightPathLengthmm);
-if ~isnan(intRow.LaserPulseEnergynJ)
-    meta(contains(meta(:,1),'experiment/laserPulseEnergy'),2) = num2cell(intRow.LaserPulseEnergynJ);
+meta(contains(meta(:,1),'experiment/pulsePeriod'),2) = num2cell((1/intRow.PulseFrequency_Hz_)*10^9);
+meta(contains(meta(:,1),'instrument/flightPathLength'),2) = num2cell(intRow.FlightPathLength_mm_);
+if ~isnan(intRow.LaserPulseEnergy_nJ_)
+    meta(contains(meta(:,1),'experiment/laserPulseEnergynJ'),2) = num2cell(intRow.LaserPulseEnergy_nJ_);
     meta(contains(meta(:,1),'experiment/pulseType'),2) = {'laserPulsed'};
 end
 meta(contains(meta(:,1),'experiment/notes'),2) = intRow.Comments;
