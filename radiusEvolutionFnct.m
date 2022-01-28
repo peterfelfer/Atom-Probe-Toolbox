e4afbff4f710e27b995b772ca1eefd627826b45e
MaWeiser <64976876+MaWeiser@users.noreply.github.com>
2022-01-28 10:56:35 +0100

    Create radiusEvolutionFnct.m
    work in progressw

---
+++ radiusEvolutionFnct.m
@@ -0,0 +1,20 @@
+function rEvo = radiusEvolutionFnct(varargin)
+
+
+% "shankAngle": shank angle (in degree)
+
+kf = 10;  % field factor
+
+Fe = 10; % evaporation field
+
+% Gormer: Radius of curvature
+R = V ./ kf ./ Fe;
+
+
+
+
+%------------------- shank angle -------------------%
+
+radEvo = (1 - sind(shankAngle)) ./ sind(shankAngle);
+
+end
