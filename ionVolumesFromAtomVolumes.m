c520a75d7ac17d51c9ad982b005f1ad61f6991ef
BenedictOtt <62551855+BenedictOtt@users.noreply.github.com>
2022-01-28 15:59:07 +0100

    ionVolumesFromAtomVolumes finished
    This Function will give you the ionVolumes of any ions in any array you want

--- ionVolumesFromAtomVolumes.m
+++ ionVolumesFromAtomVolumes.m
@@ -1,6 +1,43 @@
-function volumes = ionVolumesFromAtomVolumes(ions,isotopeTable)
+function ionVolumeList = ionVolumesFromAtomVolumes(ions,isotopeTable)
 % ionVolumesFromAtomVolumes outputs a list of ion volumes based on the ions
-% that are contained in the variable ions. This could be a categorical
-% array of ions, or a whole pos variable. The ion volumes are simple adds
-% of the individual atomic volumes as stored in the isotopeTable. These
-% volumes are based on ????
+% that are contained in the variable ions. 
+% As input parameters table, categorical or an array of elements is
+% possible.
+%
+% INPUT
+%
+% ions:             table, categorical or any other array that contains
+%                   strings/charakters
+%
+% isotopeTable:     the parsed isotope table is the basis of the atom 
+%                   density
+%
+% OUTPUT
+% 
+% ionVolumeList:    table where ionName and the depending ionVolume is
+%                   stored
+% (c) by Prof. Peter Felfer Group @FAU Erlangen-Nürnberg
+%% input validation
+    
+if istable(ions)
+    ions = categorical(ions.ion(ions.ionComplexity >= 1));
+else
+    ions = categorical(ions);
+end
+cat = categorical(categories(ions));
+ions = table(cat, 'VariableNames', {'ionName'});
+
+
+%% find AtomVolumes in isotopeTable
+for  i = 1:height(ions)
+    ionTableTest = ionConvertName(string(ions.ionName(i)));
+    k = 0;
+    for j = 1:height(ionTableTest)
+        volumeElement = mean(isotopeTable.atomDensity(isotopeTable.element == ionTableTest.element(j)));
+        k = k+volumeElement;
+    end
+    ionVolume(i,1) = k; 
+end
+
+ionVolumeList = addvars(ions, ionVolume);
+end
