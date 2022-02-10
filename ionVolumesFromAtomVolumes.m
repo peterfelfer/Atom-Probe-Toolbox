29bce88b46ceaf0bff9f860c56ccdb0e37822f5c
BenedictOtt <62551855+BenedictOtt@users.noreply.github.com>
2022-02-10 14:49:40 +0100

    Update for pos file
    Added function for pos attachment

--- ionVolumesFromAtomVolumes.m
+++ ionVolumesFromAtomVolumes.m
@@ -32,6 +32,9 @@ ions = table(cat, 'VariableNames', {'ionName'});
 
 
 %% find AtomVolumes in isotopeTable
+% include waitbar since this could take a while
+wb = waitbar(0,'building ion volumes');
+
 for  i = 1:height(ions)
     ionTableTest = ionConvertName(string(ions.ionName(i)));
     k = 0;
@@ -39,8 +42,41 @@ for  i = 1:height(ions)
         volumeElement = mean(isotopeTable.atomDensity(isotopeTable.element == ionTableTest.element(j)));
         k = k+volumeElement;
     end
-    ionVolume(i,1) = k; 
+    ionVolume(i,1) = k;
+    waitbar(i/length(ions),wb)
 end
 
-ionVolumeList = addvars(ions, ionVolume);
+ionVolumeList = addvars(ions, ionVolume); % Ionen Liste mit gewichten 
+
+%% attach ionVolumes to pos
+% for  i = 1:height(ions) % Laenge von der Liste - gehe jeden Eitnrag durch 
+%     ion = ions.ionName (i); % suche dir den ersten eintrag
+%     % k = 0; %
+%     for j = 1:height(ionVolumeList) % Laenge ion Volume liste
+%     ionVolumeForPos = ionVolumeList.ionVolume(ion == ionVolumeList.ionName (j)); % suche entsprechendes volumen 
+%     ;
+%     end
+%     ionVolumeForPosList(i,1) = k;
+% end
+% 
+% ions = addvars(ionVolumeForPosList);
+
+%% Search inside the pos/ion table for the ion and add the Volume of the ion
+h = height(ions);
+volume = zeros(h,1); 
+ions = addvars(ions, volume); % Add a zero Volume column to the pos file
+% include waitbar since this could take a while
+wb = waitbar(0,'attach ionvolumes to input file');
+
+for i = 1:height(ionVolumeList)
+    for j = 1:height(ions)
+        if ionVolumeList.ionName(i) == ions.ion(j)
+            ions.testVolumen(j) = ionVolumeList.ionVolume(i);
+        end 
+    end 
+    waitbar(i/length(ionVolumeList),wb)
+end
+
+
+    
 end
