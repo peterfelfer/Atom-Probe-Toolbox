5e854f9e9b1296ff33d045297bc1a664eceb191c
Martina <37100644+TinaHel@users.noreply.github.com>
2022-02-10 17:51:11 +0100

    function complete
    two output variable - the ionVolumeList and the pos file with the added volume - no waitbars - function needs for 4 mio atoms 3 s

--- ionVolumesFromAtomVolumes.m
+++ ionVolumesFromAtomVolumes.m
@@ -1,8 +1,10 @@
-function ionVolumeList = ionVolumesFromAtomVolumes(ions,isotopeTable)
+function [ionVolumeList, ionsOut] = ionVolumesFromAtomVolumes(ions,isotopeTable)
 % ionVolumesFromAtomVolumes outputs a list of ion volumes based on the ions
 % that are contained in the variable ions. 
 % As input parameters table, categorical or an array of elements is
 % possible.
+%
+% [ionVolumeList, ionsOut] = ionVolumesFromAtomVolumes(ions,isotopeTable);
 %
 % INPUT
 %
@@ -16,67 +18,44 @@ function ionVolumeList = ionVolumesFromAtomVolumes(ions,isotopeTable)
 % 
 % ionVolumeList:    table where ionName and the depending ionVolume is
 %                   stored
+% ionsOut:          pos/ionList table with the correspondig ion Volume for
+%                   each ion
+%
 % (c) by Prof. Peter Felfer Group @FAU Erlangen-Nürnberg
-%% input validation
+
+%% input validation -> transfer everything in table format
     
 if istable(ions)
-    ions = categorical(ions.ion);
+    cat = categorical(categories(ions.ion));
 elseif isstring(ions)
     ions = split(ions);
-    ions = categorical(ions);
+    cat = categorical(ions);
 else
-    ions = categorical(ions);
+    cat = categorical(cellstr(ions));
 end
-cat = categorical(categories(ions));
-ions = table(cat, 'VariableNames', {'ionName'});
 
+ionsCat = table(cat, 'VariableNames', {'ionName'});
 
-%% find AtomVolumes in isotopeTable
-% include waitbar since this could take a while
-wb = waitbar(0,'building ion volumes');
+%% find AtomVolumes in isotopeTable and create ionVolumeList
 
-for  i = 1:height(ions)
-    ionTableTest = ionConvertName(string(ions.ionName(i)));
+ionVolume = zeros(height(ionsCat),1);
+for i = 1:height(ionsCat)
+    ionTableTest = ionConvertName(string(ionsCat.ionName(i)));
     k = 0;
     for j = 1:height(ionTableTest)
         volumeElement = mean(isotopeTable.atomDensity(isotopeTable.element == ionTableTest.element(j)));
         k = k+volumeElement;
     end
     ionVolume(i,1) = k;
-    waitbar(i/height(ions),wb)
 end
 
-ionVolumeList = addvars(ions, ionVolume); % Ionen Liste mit gewichten 
+ionVolumeList = addvars(ionsCat, ionVolume); 
 
-%% attach ionVolumes to pos
-% for  i = 1:height(ions) % Laenge von der Liste - gehe jeden Eitnrag durch 
-%     ion = ions.ionName (i); % suche dir den ersten eintrag
-%     % k = 0; %
-%     for j = 1:height(ionVolumeList) % Laenge ion Volume liste
-%     ionVolumeForPos = ionVolumeList.ionVolume(ion == ionVolumeList.ionName (j)); % suche entsprechendes volumen 
-%     ;
-%     end
-%     ionVolumeForPosList(i,1) = k;
-% end
-% 
-% ions = addvars(ionVolumeForPosList);
+%% Join both tables and maintain the order
 
-%% Search inside the pos/ion table for the ion and add the Volume of the ion
-h = height(ions);
-volume = zeros(h,1); 
-ions = addvars(ions, volume); % Add a zero Volume column to the pos file
-% include waitbar since this could take a while
-wb = waitbar(0,'attach ionvolumes to input file');
-
-for i = 1:height(ionVolumeList)
-    for j = 1:height(ions)
-        if ionVolumeList.ionName(i) == ions.ion(j)
-            ions.testVolumen(j) = ionVolumeList.ionVolume(i);
-        end 
-    end 
-    waitbar(i/height(ionVolumeList),wb)
-end
-
+[ionsOut, order] = outerjoin(ions,ionVolumeList,"Type","left","LeftKeys","ion",...
+    "RightKeys","ionName",'RightVariables','ionVolume');
+ionsOut(order,:)=ionsOut;
 
     
 end
